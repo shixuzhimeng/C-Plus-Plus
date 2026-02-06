716421877c2242dd643b8f273c9e2fe9590b3e80
shixuzhimeng <1516446316@qq.com>
2026-02-06 23:38:49 +0800

    模板的学习

--- type/type.cpp
+++ type/type.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
 using namespace std;
 
-template<typename T>
-void Swap(T& a, T&b) {
-    T tmp = a;
-    a = b;
-    b = tmp;
-}
+//这是最早的写法
+//template<class Td>
+//这是后来的写法
+// template<typename T>
+// void Swap(T& a, T&b) {
+//     T tmp = a;
+//     a = b;
+//     b = tmp;
+// }
 
-int main() {
-    int a = 10;
-    int b = 20;
-    double c = 30.30;
-    double d = 10.10;
-    Swap(a, b);
-    Swap(c, d);
-}
+// int main() {
+//     int a = 10;
+//     int b = 20;
+//     double c = 30.30;
+//     double d = 10.10;
+//     Swap(a, b);
+//     Swap(c, d);
+
+//     return 0;
+// }
+// template <typename T1, typename T2>
+// void P (const T1& a, const T2& b) {
+//     cout << a + b <<endl;
+// }
+
+// template <class T>
+// class test {
+// public:
+//     Stack() {
+//         _array = new T[capacity];
+//         _capacity = capacity;
+//         _size = 0;
+//     }
+    
+// private:
+//     T* _array;
+//     int _capacity;
+//     int _size;
+// };
+
+// int main() {
+//     test<int> t1;
+//     test<double> t2;
+// }
