cfd42b6c7f614414ddcb5a9b54b3e8f590b1561a
shixuzhimeng <1516446316@qq.com>
2026-03-12 23:08:06 +0800

    duotai

--- Class/classjc.cpp
+++ Class/classjc.cpp
@@ -69,35 +69,172 @@ using namespace std;
 //     ~Derived() { cout << "Derived destructor" << endl; }
 // };
 
+// #include <iostream>
+// using namespace std;
+
+// class A {
+// public:
+//     int a = 10;
+// };
+
+// class B : virtual public A{
+// public:
+//     int b = 20;
+// };
+
+// class C : virtual public A{
+// public:
+//     int c = 30;
+// };
+
+// class D : public B ,public C {
+// public:
+//     int d = 40;
+// };
+
+// int main () {
+//     D d;
+//     d.a = 15;
+//     d.a = 36;
+//     cout << d.a << endl;
+//     cout << d.B::a << endl;
+//     cout << d.A::a << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// class Calculator {
+// public:
+//     int getresult(string oper) {
+//         if(oper == "+") {
+//             return num1 + num2;
+//         }
+//         else if(oper == "-") {
+//             return num1 - num2;
+//         }
+//         else if(oper == "*") {
+//             return num1 * num2;
+//         }
+//         else if(oper == "/"){
+//             return  num1 / num2;
+//         }
+//     }
+
+//     int num1;
+//     int num2;
+// };
+
+// int main () {
+//     Calculator c;
+//     c.num1 = 5;
+//     c.num2 = 1;
+//     cout << c.num1 << "+" << c.num2 << "=" << c.getresult("+") << endl; 
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// class Calculation {
+// public:
+//     virtual int getresult() {
+//         return ;
+//     }
+//     int num1;
+//     int num2;
+// };
+
+// class addCalculation : public Calculation {
+//     int getresult(){
+//         return num1 + num2;
+//     }
+    
+// };
+// class delCalculation : public Calculation {
+//     int getresult(){
+//         return num1 - num2;
+//     }
+    
+// };
+// class chengCalculation : public Calculation {
+//     int getresult(){
+//         return num1 * num2;
+//     }
+    
+// };
+// class chuCalculation : public Calculation {
+//     int getresult(){
+//         return num1 / num2;
+//     }
+    
+// };
+// int main() {
+//     //加法
+//     Calculation *a = new addCalculation;
+//     a->num1 = 1;
+//     a->num2 = 2;
+//     cout << a->num1 << "+" << a->num2 << "=" << endl;
+//     delete a;//用完后记得销毁
+//     //减法
+//     a = new delCalculation;
+//     // ...
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// class A {
+// public:
+//     virtual void a(int a) = 0;
+// };
+// class B : public A {
+// public:
+//     void a(int a) {
+//         a = 10;
+//     }
+// };
+
 #include <iostream>
 using namespace std;
 
-class A {
-public:
-    int a = 10;
+class Drink {
+    //煮水
+    virtual void Boil() = 0;
+    //冲泡
+    virtual void Brew() = 0;
+    //倒入杯中
+    virtual void PourInCup() = 0;
+    //加入辅料
+    virtual void PutSomething() = 0;
+    //制作饮品
+    virtual void MakeDrink() {
+        Boil();
+        Brew();
+        PourInCup();
+        PutSomething();
+    }
 };
 
-class B : virtual public A{
+class Coffee : public Drink {
 public:
-    int b = 20;
-};
-
-class C : virtual public A{
-public:
-    int c = 30;
-};
-
-class D : public B ,public C {
-public:
-    int d = 40;
+    virtual void Boil() {
+        cout << "煮水" << endl;
+    }
+    virtual void Brew() {
+        cout << "冲泡" << endl;
+    }
+    virtual void PourInCup() {
+        cout << "倒入杯中" << endl;
+    }
+    virtual void PutSomething() {
+        cout << "加入糖和牛奶" << endl;
+    }
 };
 
 int main () {
-    D d;
-    d.a = 15;
-    d.a = 36;
-    cout << d.a << endl;
-    cout << d.B::a << endl;
-    cout << d.A::a << endl;
     return 0;
 }
