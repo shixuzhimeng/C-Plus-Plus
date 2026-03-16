1ef691632d68fe0a6db3c841100f629286f415be
shixuzhimeng <1516446316@qq.com>
2026-03-16 23:41:50 +0800

    tiantidai

---
+++ tiantisai/2.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int x, n, y;
+//     cin >> x >> n >> y;
+//     int result = x * n + y;
+//     cout << result << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int H, h1, h2;
+//     cin >> H >> h1 >> h2;
+    
+//     cout << H << endl;
+    
+//     if (H < h1) {
+//         cout << "Bu Kan" << endl;
+//     } else if (H < h2) {  // h1 <= H < h2
+//         cout << "Zhe Gua Bao Shu Ma" << endl;
+//     } else {  // H >= h2
+//         cout << "Chi Gua" << endl;
+//     }
+    
+//     return 0;
+// }
