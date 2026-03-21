4864061e377bedbd4cb11c886b8ffbe3021b24fb
shixuzhimeng <1516446316@qq.com>
2026-03-21 23:54:09 +0800

    tiantisai

---
+++ tiantisai/4.cpp
@@ -0,0 +1,10 @@
+// #include <iostream>
+// using namespace std;
+// int main () {
+//     int sum = 0;
+//     for(int i = 0; i <= 49; i++) {
+//         sum += i;
+//     }
+//     cout << sum - 21;
+//     return 0;
+// }
